0c604f686f919c3cc0d8906d4c8c9679c50a3a34
Zachary Najda <znajda@f0r1s12.codam.nl>
2020-11-12 11:44:43 +0100

    Adding memmove and maybe an edit somewhere

--- ft_memmove/ft_memmove.c
+++ ft_memmove/ft_memmove.c
@@ -1,25 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   ft_memmove.c                                       :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: zachary <zachary@student.codam.nl>           +#+                     */
+/*   By: znajda <znajda@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/11/10 20:26:23 by zachary       #+#    #+#                 */
-/*   Updated: 2020/11/12 11:37:58 by znajda        ########   odam.nl         */
+/*   Created: 2020/11/12 11:32:01 by znajda        #+#    #+#                 */
+/*   Updated: 2020/11/12 11:44:25 by znajda        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-void *ft_memcpy(void *dest, const void *src, size_t n);
-
-int		main(void)
-{
-	char dest[50] = "LMAO THIS IS A TEST";
-	char *src = dest+14;
-	size_t n = 10;
-
-	ft_memcpy(dest,src,n);
-	printf("%s\n",dest);
-}
