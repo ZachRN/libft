97b181d5d0689028ecc6204f4552c8ec47e85452
Zachary Najda <znajda@f0r4s10.codam.nl>
2021-11-12 12:06:10 +0100

    putting ls iteri and steriteri back

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -2,14 +2,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	t_list *new;
-
-	new = lst;
 	if (!lst || !f)
 		return ;
 	while (lst)
 	{
-		(*f)(new->content);
-		new = new->next;
+		(*f)(lst->content);
+		lst = lst->next;
 	}
 }
